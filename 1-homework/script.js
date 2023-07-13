6686a65b946aaa9cdbc065a3ccd2b5dd6de92b3d
atay-gosling <138767144+atay-gosling@users.noreply.github.com>
2023-07-13 22:33:03 +0300

    Add files via upload

---
+++ 1-homework/script.js
@@ -0,0 +1,41 @@
+// let a =4
+// let b =55
+// console.log(a+b)
+
+// let a =4
+// let b =55
+// console.log (a*b)
+
+// let a =4
+// let b =55
+// console.log(a/b)
+
+// let a =4
+// let b =55
+// console.log(a%b)
+
+// let a="hello "
+// let b="Me "
+// console.log(a+b)
+
+// let a="hello "
+// let b="Baytik and Atai "
+// console.log(a+b)
+
+// let a=2
+// let b="3ll"
+// console.log(a*b)
+
+// let a=5
+// let b="5"
+// console.log(Number(b)+a)
+
+// let a=5
+// let b="5"
+// console.log(a+ +b)
+
+// let c=2
+// console.log(c)
+//
+// let b
+// console.log(b)
